d9e093fc97dc1da1058a251023434a10be334599
ayandu <nbc.555.an@gmail.com>
2019-10-27 19:05:53 +0200

    only remain with bookshop-api/bookshop which represents monolithic style

--- order/src/main/java/com/bookshop/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookshop.order.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.bookshop.order.model.Order;
-
-public interface OrderRepository extends ReactiveMongoRepository<Order, String> {
-
-}
--- shopping-cart/src/main/java/com/bookshop/cart/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookshop.cart.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.bookshop.cart.model.Cart;
-
-public interface CartRepository extends ReactiveMongoRepository<Cart, String> {
-}
